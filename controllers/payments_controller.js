c077c20fa0e86e7c4169aa93ac3d01df52f0acf4
Nicholas Kneafsey <nicholaskneafsey@gmail.com>
2016-05-20 15:25:01 -0700

    Made it so that payments and bills can be created

--- controllers/payments_controller.js
+++ controllers/payments_controller.js
@@ -12,13 +12,31 @@ module.exports = {
     invokeModel(req, res, params, paymentsModel, 'getWhatHasBeenPaidToUser');
   },
 
-  post: function(req, res) {
-    const params = [req.body.billId, req.body.userId, req.body.amount];
-    invokeModel(req, res, params, paymentsModel, 'post');
-  },
-
   markPaymentAsPaid: function(req, res) {
     const params = [req.params.paymentId];
     invokeModel(req, res, params, paymentsModel, 'markPaymentAsPaid');
+  },
+
+  post: function(req, res) {
+    const userPromises = req.body.data.users.map(function(user) {
+      return new Promise(function(resolve, reject) {
+        const params = [req.insertId, user.id, user.amount];
+        paymentsModel.post(params, function (err, results) {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(results)
+          }
+        });
+      })
+    });
+
+    Promise.all(userPromises).then(function(results) {
+      console.log("RESULTS", results);
+      res.sendStatus(201);
+    }).catch(function(err) {
+      console.error(err);
+      res.sendStatus(500);
+    });
   }
 }
